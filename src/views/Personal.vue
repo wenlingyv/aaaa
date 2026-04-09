3a53e0c0ce717197fa7916b1bf00d83e9e8356a4
zmh <3128089433@qq.com>
2026-04-09 17:45:01 +0800

    feat 全屏

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,7 +1,35 @@
 <template>
-  <div class="page">个人中心</div>
+  <div class="personal-page">
+    <h2>个人中心</h2>
+    <button @click="toggleFullscreen" class="fullscreen-btn">
+      {{ isFullscreen ? '退出全屏' : '全屏模式' }}
+    </button>
+  </div>
 </template>
 
-<style scoped>
-.page { font-size: 22px; padding: 20px; }
-</style>
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import '../style/Personal.css'
+
+const isFullscreen = ref(false)
+
+const toggleFullscreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
+}
+
+const handleFullscreenChange = () => {
+  isFullscreen.value = !!document.fullscreenElement
+}
+
+onMounted(() => {
+  document.addEventListener('fullscreenchange', handleFullscreenChange)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('fullscreenchange', handleFullscreenChange)
+})
+</script>
